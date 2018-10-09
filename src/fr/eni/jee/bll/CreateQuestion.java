32d21dc4c77dad8b6e5e46dd3c8725c5313f112d
Unknown <a.delaunay97@gmail.com>
2018-10-09 16:47:06 +0200

    update for switch

--- src/fr/eni/jee/bll/CreateQuestion.java
+++ src/fr/eni/jee/bll/CreateQuestion.java
@@ -2,11 +2,10 @@ package fr.eni.jee.bll;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+
 
 /**
  * Servlet implementation class CreateQuestion
