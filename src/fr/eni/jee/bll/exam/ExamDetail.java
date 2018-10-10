6aaf32c6c1b7d690b2355e1e2aaa483a57ddae09
Adrien Guillement <adrien.guillement@gmail.com>
2018-10-10 16:25:41 +0200

    ExamDetail servlet

--- src/fr/eni/jee/bll/exam/ExamDetail.java
+++ src/fr/eni/jee/bll/exam/ExamDetail.java
@@ -1,23 +1,29 @@
 package fr.eni.jee.bll.exam;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.eni.jee.bo.Exam;
+import fr.eni.jee.bo.Question;
+import fr.eni.jee.dal.EpreuveDAO;
+
 /**
  * Servlet implementation class Exam
  */
 @WebServlet("/Exam")
-public class Exam extends HttpServlet {
+public class ExamDetail extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Exam() {
+    public ExamDetail() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -27,7 +33,15 @@ public class Exam extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		System.out.println(request.getParameter("id"));
+		int examID = Integer.parseInt(request.getParameter("id"));
+		try {
+			Exam exam = EpreuveDAO.SearchByID(examID);
+			request.setAttribute("exam", exam);
+			List<Question> question = new ArrayList<Question>();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		this.getServletContext().getRequestDispatcher("/WEB-INF/ManageTest/Exam.jsp").forward( request, response );
 	}
 
