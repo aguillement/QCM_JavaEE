3c58ae439129efbc89a91520bbe0e3e5659104b1
Unknown <a.delaunay97@gmail.com>
2018-10-10 16:25:22 +0200

    Show question

--- src/fr/eni/jee/bll/ManagementQuestion/ShowQuestion.java
+++ src/fr/eni/jee/bll/ManagementQuestion/ShowQuestion.java
@@ -1,11 +1,18 @@
 package fr.eni.jee.bll.ManagementQuestion;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.jee.bo.Question;
+import fr.eni.jee.dal.QuestionDAO;
 
 /**
  * Servlet implementation class ShowQuestion
@@ -13,29 +20,39 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ShowQuestion")
 public class ShowQuestion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String VIEW = "/WEB-INF/ManagementQuestion/QuestionView.jsp";
        
     /**
      * @see HttpServlet#HttpServlet()
      */
     public ShowQuestion() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		List<Question> lstQuestion = new ArrayList<Question>();
+		
+		try {
+			lstQuestion = QuestionDAO.GetAll();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		request.setAttribute("lstQuestion", lstQuestion);
+		
+		this.getServletContext().getRequestDispatcher( VIEW ).forward( request, response );
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		this.getServletContext().getRequestDispatcher( VIEW ).forward( request, response );
 	}
 
 }
